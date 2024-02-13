bdb1d54b0a770d3e85fffc303275292e986a5f79
Your Name <you@example.com>
2024-02-13 16:24:48 +0300

    123

---
+++ WpfApp1/WelcomPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomPage.xaml
+    /// </summary>
+    public partial class WelcomPage : Window
+    {
+        public WelcomPage()
+        {
+            InitializeComponent();
+            Piska.ItemsSource = bolEntities.GetContext().users.ToList();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)//Связь с таблицей сотрудники
+        {
+            using (var db = new bolEntities())
+            {
+                Piska.ItemsSource = db.users.ToList();
+            }
+        }
+    }
+}
